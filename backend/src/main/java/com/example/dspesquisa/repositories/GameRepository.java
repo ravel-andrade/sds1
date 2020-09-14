eaff247f952c516f0fc3932a451c75d180d4c738
Ravel Andrade <ravel.gou@gmail.com>
2020-09-14 18:23:21 -0300

    Entities, repositories

---
+++ backend/src/main/java/com/example/dspesquisa/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.example.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.dspesquisa.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+	
+	
+}
---
+++ backend/src/main/java/com/example/dspesquisa/repositories/GenreRepository.java
@@ -0,0 +1,11 @@
+package com.example.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.dspesquisa.entities.Genre;
+
+public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+	
+	
+}
