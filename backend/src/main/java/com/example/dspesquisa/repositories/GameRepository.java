dc9ef70cc36a49e200f45e410516644898324992
Ravel Andrade <ravel.gou@gmail.com>
2020-09-14 19:12:02 -0300

    DTO, Service

--- backend/src/main/java/com/example/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/example/dspesquisa/repositories/GameRepository.java
@@ -1,11 +1,12 @@
 package com.example.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.dspesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 
-	
 	
 }
