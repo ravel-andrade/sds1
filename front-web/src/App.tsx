ed024f75b0af66ef6027056fc834c5fd9b9d7c37
Ravel Andrade <ravel.gou@gmail.com>
2020-09-17 16:51:52 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1>Hello word</h1>
+     <div>
+       <Header />
+      <Home />
+     </div>
+     
   );
 }
 
