4733356be9e341647c9f9f794bbedbe3b8aa91a8
Ravel Andrade <ravel.gou@gmail.com>
2020-09-19 18:10:35 -0300

    Adicionando pagina home no react native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {FontAwesome5 as Icon} from '@expo/vector-icons';
-import {Text, StyleSheet, View, Image, Button} from 'react-native';
+import {Text, StyleSheet, View, Image, Alert} from 'react-native';
 import {RectButton} from 'react-native-gesture-handler'
 
 const Home = () =>{
+
+    const handleOnPress = () =>{
+        
+    }
+
     return(
         <>
         <View style={styles.container}>
@@ -15,7 +20,7 @@ const Home = () =>{
             <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
         </View>
         <View style={styles.footer}>
-            <RectButton style={styles.button}>
+            <RectButton style={styles.button} onPress={handleOnPress}>
                 <Text style={styles.buttonText}>COLETAR DADOS</Text>
                 <View style={styles.buttonIcon}>
                     <Text>
